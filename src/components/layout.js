95b980018069bf00c6da3d64043d1fdc6400d4f9
Karol Dobrek <Inakus@wp.pl>
2023-04-08 17:15:07 +0200

    added page title

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,13 @@
 import Navbar from "./navbar";
 import Footer from "./footer";
+import Head from "next/head";
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <title>3D Chart App</title>
+      </Head>
       <div className="flex flex-col h-screen justify-between">
         <Navbar />
         <main>{children}</main>
